332d01e476385ac35ac9dd6de7fd94ae3d147ece
frideldev <fridel@gmail.com>
2021-07-10 11:03:02 -0400

    Galeria con Opciones, Optimizacion imagenes

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ let navigation = document.querySelector('.navigation');
 let main = document.querySelector('.main');
 let themeSwitch = document.querySelector('.themeSwitch');
 let body = document.querySelector('body');
+let list = document.querySelectorAll('.list');
+let workBx=document.querySelectorAll('.workBx');
 toggle.onclick = ()=>{
     toggle.classList.toggle('active');
     topbar.classList.toggle('active');
@@ -20,4 +22,23 @@ function toogleMenu(){
 let main = document.querySelector('.main');
 navigation.classList.remove('active');
 main.classList.remove('active');
+}
+
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click',()=>{
+        for (let j = 0; j < list.length; j++) {
+            list[j].classList.remove('active');
+        }
+        list[i].classList.add('active');
+        let dataFilter=list[i].getAttribute('data-filter');
+        for (let k = 0; k < workBx.length; k++) {
+            workBx[k].classList.remove('active');
+            workBx[k].classList.add('hide');
+            if(workBx[k].getAttribute('data-item')==dataFilter||dataFilter == "all"){
+                workBx[k].classList.remove('hide'); 
+                workBx[k].classList.add('active'); 
+            }
+        }
+    });
+    
 }
